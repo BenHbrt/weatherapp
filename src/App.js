aae6ed0c92a911af269656eee03ca432be07f867
BenHbrt <benherbert1983@googlemail.com>
2023-03-27 19:34:14 +0200

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Main from './Components/Main';
 import Credits from './Components/Credits';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/credits" element={<Credits />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
